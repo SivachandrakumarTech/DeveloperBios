89f9adca0b31b0ae38cf9b66744f3bb4c565c841
Sivachandrakumar Chandrasekaran <sivachandrakumar.c@gmail.com>
2024-08-28 12:23:51 -0400

    Made changes using context

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './Styles/index.css';
 import App from './Components/App';
 import reportWebVitals from './reportWebVitals';
+import AuthContextProvider from './Context/AuthContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
     <App />
+    </AuthContextProvider>    
   </React.StrictMode>
 );
 
